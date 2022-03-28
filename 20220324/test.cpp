746145db7b827a3394a00a5f43a84092aaa8dfdd
Gyuho Cha <hwoe0205@naver.com>
2022-03-28 18:33:30 +0900

    20220324 2nd commit

--- 20220324/test.cpp
+++ 20220324/test.cpp
@@ -4,13 +4,17 @@
 
 int main(int argc, char* argv[])
 {	
+	float a, b;
+	a = atof(argv[1]);
+	b = atof(argv[2]);
+	
 	printf("argc = %d\n",argc);
 	printf(" %s\n",argv[0]);
 	
-	printf("%d\n",atoi(argv[1])+atoi(argv[2]));
-	printf("%d\n",atoi(argv[1])-atoi(argv[2]));
-	printf("%d\n",atoi(argv[1])*atoi(argv[2]));
-	printf("%d\n",atoi(argv[1])/atoi(argv[2]));
+	printf("%f + %f = %f\n", a, b, a+b);
+	printf("%f - %f = %f\n", a, b, a-b);
+	printf("%f * %f = %f\n", a, b, a*b);
+	printf("%f / %f = %f\n", a, b, a/b);
 	
  	return 1;
 }
